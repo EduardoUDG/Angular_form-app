90bbb34327b1b52d68f6ad54f2dabf464284465c
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-07-02 13:37:51 -0500

    update - interface Person, Favorites and ngModel to input controls

--- src/app/template/dinamics/dinamics.component.ts
+++ src/app/template/dinamics/dinamics.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Person {
+  name: string;
+  favorites: Favorite[]
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styles: [
-  ]
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
   @ViewChild('myForm') myForm!: NgForm;
 
+  person: Person = {
+    name: 'Eduardo',
+    favorites: [
+      { id: 1, name: 'GOW' },
+      { id: 2, name: 'HALO' },
+    ]
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
   save() {
     console.log(this.myForm);
