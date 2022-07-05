2c5ae8a866e311d4fe3f37d7af4647d735ef202a
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-07-04 20:10:26 -0500

    update - null, *ngIf

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -16,14 +16,20 @@ export class BasicsComponent {
   // });
 
   myForm: FormGroup = this._fb.group({
-    name  : ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
-    price : [0, [Validators.required, Validators.min(0)]],
-    stock : [0, [Validators.required, Validators.min(0)]],
+    name  : [ , [Validators.required, Validators.minLength(3)]],
+    price : [ , [Validators.required, Validators.min(0)]],
+    stock : [ , [Validators.required, Validators.min(0)]],
   });
 
 
   constructor(
     private _fb: FormBuilder
   ) { }
+
+  invalidInput( inputName: string ) {
+    return this.myForm.controls[`${inputName}`].errors
+            && this.myForm.controls[`${inputName}`].touched
+  }
+
 
 }
