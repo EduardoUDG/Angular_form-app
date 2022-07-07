6a28ce8a696abface8de07b1c5b74d4d865915c9
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-07-06 23:25:44 -0500

    update - reactive dinamics, validate input name from scratch
    update - reactive dinamics, validate input name from scratch

--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dinamics',
@@ -8,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this._fb.group({
+    name: [ '', [Validators.required, Validators.minLength(3)] ],
+  })
+
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  inputValidate( inputName:string ) {
+    return this.myForm.controls[`${inputName}`].errors
+            && this.myForm.controls[`${inputName}`].touched;
+  }
+
+  save() {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.myForm.value );
   }
 
 }
